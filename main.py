5b4f42699ef8f0f124d758950039150e484408b0
Pavel Rabekin <pavelrabekin@gmail.com>
2023-04-28 11:00:06 +0300

    fixed 3

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def main():
     application.add_handler(edit_product_handler)
     application.add_handler(add_user_handler)
 
-    # application.run_polling()
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=5000,
-        secret_token=os.environ.get("SECRET_TOKEN"),
-        webhook_url=os.environ.get("WEBHOOK_URL")
-    )
+    application.run_polling(poll_interval=5)
+    # application.run_webhook(
+    #     listen="0.0.0.0",
+    #     port=5000,
+    #     secret_token=os.environ.get("SECRET_TOKEN"),
+    #     webhook_url=os.environ.get("WEBHOOK_URL")
+    # )
 
 
 if __name__ == "__main__":

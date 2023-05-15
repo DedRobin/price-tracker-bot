f1685daccad931e7f48a333f4968b7b5b3e709eb
Pavel Rabekin <pavelrabekin@gmail.com>
2023-05-15 13:35:50 +0300

    the price is displayed as a number with two decimal places (x.xx)

--- source/bot/jobs.py
+++ source/bot/jobs.py
@@ -18,22 +18,26 @@ async def send_notifications(context: ContextTypes.DEFAULT_TYPE):
             url = product.product_link
 
             # Received a product price by URL
-            _, price = await onliner.parse(session, url)
+            _, new_price = await onliner.parse(session, url)
 
-            if price != product.current_price:
-                updated_product = await update_product(product=product, price=price)
+            if new_price != product.current_price:
+                updated_product = await update_product(product=product, price=new_price)
                 chat_ids = [user.chat_id for user in updated_product.users]
 
+                # Product data
                 current_price = updated_product.current_price
                 previous_price = updated_product.previous_price
                 link = updated_product.product_link
                 name = updated_product.name
 
+                # Count a difference
                 different = current_price - previous_price
-                word = "снизилась" if different < 0 else "выросла" ""
-                emoji = "\U0001F601" if different < 0 else "\U0001F621"
+                different = round(different, 2)
+                if abs(different) >= 1:
+                    word = "снизилась" if different < 0 else "выросла"
+                    emoji = "\U0001F601" if different < 0 else "\U0001F621"
 
-                notification_text = f"""{link}
+                    notification_text = f"""{link}
     
 {emoji}{name}
     
@@ -41,9 +45,11 @@ async def send_notifications(context: ContextTypes.DEFAULT_TYPE):
 Предыдущая цена = {previous_price} BYN
 Новая цена = {current_price} BYN"""
 
-                for chat_id in chat_ids:
-                    await context.bot.send_message(
-                        chat_id=chat_id, text=notification_text
-                    )
+                    for chat_id in chat_ids:
+                        await context.bot.send_message(
+                            chat_id=chat_id, text=notification_text
+                        )
+                else:
+                    pass
 
         logger.info("All notifications have been sent")
